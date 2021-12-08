7804b5556aa03bc2214632b6970ab4fbd2a17853
msantillan72 <msantillan72@gmail.com>
2021-12-08 00:24:02 -0600

    mi quinto commit

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express= require('express');
 const app=express();
 app.use(express.static('public'));
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(process.env.PORT || 3000 , function() {
+    console.log('Servidor corriendo en el puerto 3000');
 });
 
 app.get('/',(req,res)=>{
